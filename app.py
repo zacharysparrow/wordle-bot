63fd7ff56718af459060052cfcc2510d8ef6de63
Zachary Sparrow <zachsparrow95@gmail.com>
2025-04-11 19:31:23 -0400

    bug fixes

--- app.py
+++ app.py
@@ -3,13 +3,6 @@ from utils import wordle_solve
 
 app = Flask(__name__)
 
-def read_text_file(filepath):
-    try:
-        with open(filepath, 'r') as file:
-            return file.read()
-    except FileNotFoundError:
-        return "File not found."
-
 @app.route('/', methods=['GET', 'POST'])
 def index():
     dict5 = []
@@ -17,8 +10,8 @@ def index():
         for line in f:
             dict5.append(line.strip().split(',')[0])
         if request.method == 'POST':
-            hidden_word = request.form['hidden_word']
-            start_word = request.form['start_word']
+            hidden_word = request.form['hidden_word'].lower()
+            start_word = request.form['start_word'].lower()
             if len(hidden_word) != 5:
                 return render_template('index.html', output=f'Please choose a 5-letter long hidden word!')
             if len(start_word) != 5 and start_word != '':
--- init_words.py
+++
@@ -1,4 +0,0 @@
-dict5 = []
-with open('assets/words.txt') as f:
-    for line in f:
-        dict5.append(line.strip().split(',')[0])
