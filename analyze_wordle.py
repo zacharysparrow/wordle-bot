aeeb0d69828b870bb0f6690188bf61f9169ce470
Zachary Sparrow <zachsparrow95@gmail.com>
2025-05-24 10:48:06 -0400

    solution analysis

--- analyze_wordle.py
+++ analyze_wordle.py
@@ -1,4 +1,7 @@
 from utils import *
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
 
 dict5 = []
 with open('assets/words.txt') as f:
@@ -7,7 +10,41 @@ with open('assets/words.txt') as f:
 
 all_solns = []
 for word in dict5:
-    soln = wordle_solve('solve', 'reals')
+    soln = wordle_solve(word, 'reals')
     all_solns.append(soln)
 
-print(all_solns[0:5])
+depths = []
+for soln in all_solns:
+    depths.append(len(soln))
+
+soln_counts = count_occurrences(depths)
+print(soln_counts)
+
+mean_depth = np.mean(depths)
+
+plt.figure(figsize=(10, 6))
+hist = sns.histplot(data=depths, bins=6, discrete=True)
+y_bottom, y_top = hist.get_ylim()
+padding = (y_top - y_bottom) * 0.1
+hist.set_ylim(y_bottom, y_top + padding)
+text_place = np.mean(sorted(hist.get_yticks())[0:-1])
+
+plt.axvline(mean_depth, 0, 1, color='black')
+hist.annotate("mean: "+'{0:.2f}'.format(mean_depth), xy=(mean_depth + 0.1,text_place), horizontalalignment = 'center', rotation=270)
+for p in hist.patches:
+    number_label = int(p.get_height())/len(depths)
+    hist.annotate(
+        f'{number_label:.1%}',
+        (p.get_x() + p.get_width() / 2., p.get_height()),
+        ha='center',
+        va='center',
+        xytext=(0, 10),
+        textcoords='offset points'
+    )
+
+plt.title('Number of Guesses to Solve Wordle')
+plt.xlabel('# Guesses')
+plt.ylabel('Frequency')
+plt.grid(False)
+plt.show()
+
